00a059c61238b5b52985d2aba835e6d6fbc19023
ChristineXYS <flychristine@gmail.com>
2016-07-12 14:34:42 +0800

    Complete all pages.

--- XamHawkDemo/XamHawkDemo/FeedbackPage.xaml.cs
+++ XamHawkDemo/XamHawkDemo/FeedbackPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
+using StreetHawkCrossplatform;
+
 namespace XamHawkDemo
 {
 	public partial class FeedbackPage : ContentPage
@@ -10,6 +12,28 @@ namespace XamHawkDemo
 		public FeedbackPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			DependencyService.Get<IStreetHawkAnalytics>().NotifyViewEnter(this.GetType().Name);
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			DependencyService.Get<IStreetHawkAnalytics>().NotifyViewExit(this.GetType().Name);
+		}
+
+		private void buttonSubmitClick(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(this.textboxTitle.Text))
+			{
+				DisplayAlert("Please input title.", "Title is mandatory for feedback.", "OK");
+				return;
+			}
+			DependencyService.Get<IStreetHawkAnalytics>().SendSimpleFeedback(this.textboxTitle.Text, this.textboxMessage.Text);
 		}
 	}
 }
--- XamHawkDemo/XamHawkDemo/InstallInfoPage.xaml.cs
+++ XamHawkDemo/XamHawkDemo/InstallInfoPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
+using StreetHawkCrossplatform;
+
 namespace XamHawkDemo
 {
 	public partial class InstallInfoPage : ContentPage
@@ -10,6 +12,26 @@ namespace XamHawkDemo
 		public InstallInfoPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			DependencyService.Get<IStreetHawkAnalytics>().NotifyViewEnter(this.GetType().Name);
+
+			this.labelAppKey.Text = string.Format("App key: {0}", DependencyService.Get<IStreetHawkAnalytics>().GetAppKey());
+			this.labelInstallId.Text = string.Format("Install id: {0}", DependencyService.Get<IStreetHawkAnalytics>().GetInstallId());
+			this.labelEnableLog.Text = string.Format("Enable console log: {0}", DependencyService.Get<IStreetHawkAnalytics>().GetEnableLogs());
+			this.labelSDKLibrary.Text = string.Format("SDK version: {0}", DependencyService.Get<IStreetHawkAnalytics>().GetSHLibraryVersion());
+			this.labelAdvertisement.Text = string.Format("Advertisement id: {0}", DependencyService.Get<IStreetHawkAnalytics>().GetAdvertisementId());
+			this.labeliTunesId.Text = string.Format("iTunes id (iOS Specific): {0}", DependencyService.Get<IStreetHawkAnalytics>().GetInstallId());
+			this.labelGCMId.Text = string.Format("GCM id (Android Specific): {0}", DependencyService.Get<IStreetHawkPush>().GetGcmSenderId());
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			DependencyService.Get<IStreetHawkAnalytics>().NotifyViewExit(this.GetType().Name);
 		}
 	}
 }
